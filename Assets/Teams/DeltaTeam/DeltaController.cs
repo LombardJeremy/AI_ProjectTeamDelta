a6ad7f3f38f01f5304c35f42fd412ab7a323e73e
Volod_IA <volodiabussereau@gmail.com>
2025-11-03 15:17:06 +0100

    [ADD] DeltaTeam folder with Controller

---
+++ Assets/Teams/DeltaTeam/DeltaController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DoNotModify;
+
+namespace DeltaTeam {
+
+	public class DeltaController : BaseSpaceShipController
+	{
+
+		public override void Initialize(SpaceShipView spaceship, GameData data)
+		{
+		}
+
+		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
+		{
+			SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
+			float thrust = 1.0f;
+			float targetOrient = otherSpaceship.Orientation;
+			bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
+			return new InputData(thrust, targetOrient, needShoot, false, false);
+		}
+	}
+
+}
