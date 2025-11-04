d315111600f2df3a98267cb8d09f9a59beb8247e
Volod_IA <volodiabussereau@gmail.com>
2025-11-04 23:33:18 +0100

    [ADD] GetTimeSinceLastAction

--- Assets/Teams/DeltaTeam/DeltaController.cs
+++ Assets/Teams/DeltaTeam/DeltaController.cs
@@ -5,6 +5,20 @@ using DoNotModify;
 
 namespace DeltaTeam {
 
+	public struct TimeSinceLastAction
+	{
+		
+		public float Shoot;
+		public float DropMine;
+		public float Shockwave;
+
+		public TimeSinceLastAction(bool b)
+		{
+			Shoot = -1000f;
+			DropMine = -1000f;
+			Shockwave = -1000f;
+		}
+	}
 	public class DeltaController : BaseSpaceShipController
 	{
 		public InputData InputData;
@@ -12,11 +26,13 @@ namespace DeltaTeam {
 		public SpaceShipView OwnSpaceShip;
 		public GameData GameData;
 		public GameObject PositionPreviewPrefab;
+		public TimeSinceLastAction TimeSinceLastAction;
 		public override void Initialize(SpaceShipView spaceship, GameData data)
 		{
 			OtherSpaceShip = data.GetSpaceShipForOwner(1 - spaceship.Owner);
 			OwnSpaceShip = spaceship;
 			GameData = data;
+			TimeSinceLastAction = new TimeSinceLastAction(true);
 		}
 
 		public void PreviewPosition(Vector2 position)
@@ -28,11 +44,17 @@ namespace DeltaTeam {
 		public override InputData UpdateInput(SpaceShipView spaceship, GameData data)
 		{
 			GameData = data;
+			
+			if (InputData.shoot) TimeSinceLastAction.Shoot = Time.time;
+			if (InputData.dropMine) TimeSinceLastAction.DropMine = Time.time;
+			if (InputData.fireShockwave) TimeSinceLastAction.Shockwave = Time.time;
+			
 			/*SpaceShipView otherSpaceship = data.GetSpaceShipForOwner(1 - spaceship.Owner);
 			float thrust = 1.0f;
 			float targetOrient = otherSpaceship.Orientation;
 			bool needShoot = AimingHelpers.CanHit(spaceship, otherSpaceship.Position, otherSpaceship.Velocity, 0.15f);
 			return new InputData(thrust, targetOrient, needShoot, false, false);*/
+
 			return InputData;
 		}
 	}
---
+++ Assets/Teams/DeltaTeam/Tasks/Actions/InfosGetters/GetTimeSinceLastAction.cs
@@ -0,0 +1,33 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+using UnityEngine;
+
+namespace DeltaTeam.Tasks.Actions
+{
+    [TaskCategory("DeltaTeam")]
+    public class GetTimeSinceLastAction : Action
+    {
+        public SharedDeltaController SharedController;
+        public SharedFloat TimeSinceLastShot;
+        public SharedFloat TimeSinceLastMineDropped;
+        public SharedFloat TimeSinceLastFiredShockwave;
+
+        public override string OnDrawNodeText()
+        {
+            if (SharedController.Value == null) { return "Missing Controller"; }
+            return base.OnDrawNodeText();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            TimeSinceLastAction timeSinceLastAction = SharedController.Value.TimeSinceLastAction;
+            
+            TimeSinceLastShot.Value = Time.time - timeSinceLastAction.Shoot;
+            TimeSinceLastFiredShockwave.Value = Time.time - timeSinceLastAction.Shockwave;
+            TimeSinceLastMineDropped.Value = Time.time - timeSinceLastAction.DropMine;
+            
+            return TaskStatus.Success;
+        }
+    }
+}
