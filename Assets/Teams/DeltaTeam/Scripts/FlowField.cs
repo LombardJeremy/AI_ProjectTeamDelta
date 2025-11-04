465f66b7baeb84799e9509037fe78f978c37e88e
jeremy-lombard <jeremylombard.mail@gmail.com>
2025-11-04 15:26:26 +0100

    Flow Field Implementet
    Not Working With SpaceShip

---
+++ Assets/Teams/DeltaTeam/Scripts/FlowField.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DoNotModify;
+using UnityEngine;
+
+public class FlowField
+{
+	public Cell[,] grid { get; private set; }
+	public Vector2Int gridSize { get; private set; }
+	public float cellRadius { get; private set; }
+	public Cell destinationCell;
+
+	private float cellDiameter;
+
+	private List<AsteroidView> listOfAsteroid;
+
+	public FlowField(float _cellRadius, Vector2Int _gridSize)
+	{
+		cellRadius = _cellRadius;
+		cellDiameter = cellRadius * 2f;
+		gridSize = _gridSize;
+	}
+
+	public void CreateGrid()
+	{
+		listOfAsteroid = GameManager.Instance.GetGameData().Asteroids;
+		grid = new Cell[gridSize.x, gridSize.y];
+
+		for (int x = 0; x < gridSize.x; x++)
+		{
+			for (int y = 0; y < gridSize.y; y++)
+			{
+				Vector3 worldPos = new Vector3(cellDiameter * x + cellRadius + GridController.GridControllerInstance.gridOffset.x, 
+					cellDiameter * y + cellRadius + GridController.GridControllerInstance.gridOffset.y, 0);
+				grid[x, y] = new Cell(worldPos, new Vector2Int(x, y));
+			}
+		}
+	}
+
+	public void CreateCostField()
+	{
+		Vector3 cellHalfExtents = Vector3.one * cellRadius;
+		cellHalfExtents.z = 0;
+		int terrainMask = LayerMask.GetMask("Asteroid");
+		foreach (Cell curCell in grid)
+		{
+			if (listOfAsteroid.Count != 0)
+			{
+				foreach (var asteroid in listOfAsteroid)
+				{
+					float asteroidRadius = asteroid.Radius;
+					Vector2 center = asteroid.Position;
+					Cell asteroidCellCenter = GetCellFromWorldPos(center);
+					if (curCell.worldPos.x <= asteroidCellCenter.worldPos.x + asteroidRadius &&
+					    curCell.worldPos.x >= asteroidCellCenter.worldPos.x - asteroidRadius && 
+					    curCell.worldPos.y <= asteroidCellCenter.worldPos.y + asteroidRadius &&
+					    curCell.worldPos.y >= asteroidCellCenter.worldPos.y - asteroidRadius)
+					{
+						curCell.IncreaseCost(255);
+					}
+				}
+			}
+			
+			Collider[] obstacles = Physics.OverlapBox(curCell.worldPos, cellHalfExtents, Quaternion.identity, terrainMask);
+			bool hasIncreasedCost = false;
+			foreach (Collider col in obstacles)
+			{
+				if (col.gameObject.layer == LayerMask.GetMask("Asteroid"))
+				{
+					curCell.IncreaseCost(255);
+					continue;
+				}
+				else if (!hasIncreasedCost && col.gameObject.layer == 9)
+				{
+					curCell.IncreaseCost(3);
+					hasIncreasedCost = true;
+				}
+			}
+		}
+	}
+
+	public void CreateIntegrationField(Cell _destinationCell)
+	{
+		destinationCell = _destinationCell;
+
+		destinationCell.cost = 0;
+		destinationCell.bestCost = 0;
+
+		Queue<Cell> cellsToCheck = new Queue<Cell>();
+
+		cellsToCheck.Enqueue(destinationCell);
+
+		while(cellsToCheck.Count > 0)
+		{
+			Cell curCell = cellsToCheck.Dequeue();
+			List<Cell> curNeighbors = GetNeighborCells(curCell.gridIndex, GridDirection.CardinalDirections);
+			foreach (Cell curNeighbor in curNeighbors)
+			{
+				if (curNeighbor.cost == byte.MaxValue) { continue; }
+				if (curNeighbor.cost + curCell.bestCost < curNeighbor.bestCost)
+				{
+					curNeighbor.bestCost = (ushort)(curNeighbor.cost + curCell.bestCost);
+					cellsToCheck.Enqueue(curNeighbor);
+				}
+			}
+		}
+	}
+
+	public void CreateFlowField()
+	{
+		foreach(Cell curCell in grid)
+		{
+			List<Cell> curNeighbors = GetNeighborCells(curCell.gridIndex, GridDirection.AllDirections);
+
+			int bestCost = curCell.bestCost;
+
+			foreach(Cell curNeighbor in curNeighbors)
+			{
+				if(curNeighbor.bestCost < bestCost)
+				{
+					bestCost = curNeighbor.bestCost;
+					curCell.bestDirection = GridDirection.GetDirectionFromV2I(curNeighbor.gridIndex - curCell.gridIndex);
+				}
+			}
+		}
+	}
+
+	private List<Cell> GetNeighborCells(Vector2Int nodeIndex, List<GridDirection> directions)
+	{
+		List<Cell> neighborCells = new List<Cell>();
+
+		foreach (Vector2Int curDirection in directions)
+		{
+			Cell newNeighbor = GetCellAtRelativePos(nodeIndex, curDirection);
+			if (newNeighbor != null)
+			{
+				neighborCells.Add(newNeighbor);
+			}
+		}
+		return neighborCells;
+	}
+
+	private Cell GetCellAtRelativePos(Vector2Int orignPos, Vector2Int relativePos)
+	{
+		Vector2Int finalPos = orignPos + relativePos;
+
+		if (finalPos.x < 0 || finalPos.x >= gridSize.x || finalPos.y < 0 || finalPos.y >= gridSize.y)
+		{
+			return null;
+		}
+
+		else { return grid[finalPos.x, finalPos.y]; }
+	}
+
+	public Cell GetCellFromWorldPos(Vector3 worldPos)
+	{
+		Vector2 offset = GridController.GridControllerInstance.gridOffset;
+
+		float percentX = (worldPos.x - offset.x) / (gridSize.x * cellDiameter);
+		float percentY = (worldPos.y - offset.y) / (gridSize.y * cellDiameter);
+
+		percentX = Mathf.Clamp01(percentX);
+		percentY = Mathf.Clamp01(percentY);
+
+		int x = Mathf.Clamp(Mathf.FloorToInt(gridSize.x * percentX), 0, gridSize.x - 1);
+		int y = Mathf.Clamp(Mathf.FloorToInt(gridSize.y * percentY), 0, gridSize.y - 1);
+
+		return grid[x, y];
+	}
+
+}
---
+++ Assets/Teams/DeltaTeam/Scripts/GridController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DeltaTeam;
+using DoNotModify;
+using UnityEngine;
+
+public class GridController : MonoBehaviour
+{
+    public Vector2Int gridSize;
+    public float cellRadius = 0.5f;
+    public FlowField curFlowField;
+	public GridDebug gridDebug;
+	public GameData gameData;
+
+	public Vector2Int gridOffset;
+
+	public static GridController GridControllerInstance
+	{
+		get ;
+		set ;
+	}
+
+	private void Start()
+	{
+		if (GridControllerInstance == null)
+		{
+			GridControllerInstance = this;
+		}
+		else
+		{
+			Destroy(this);
+		}
+
+		gameData = GameManager.Instance.GetGameData();
+	}
+
+
+	private void InitializeFlowField()
+	{
+        curFlowField = new FlowField(cellRadius, gridSize);
+        curFlowField.CreateGrid();
+		gridDebug.SetFlowField(curFlowField);
+	}
+    
+
+	private void Update()
+	{
+		
+		if (gameData == null && GameManager.Instance.GetGameData() != null)
+		{
+			gameData = GameManager.Instance.GetGameData();
+		}
+
+		if (Input.GetKey(KeyCode.Q))
+		{
+			InitializeFlowField();
+
+			curFlowField.CreateCostField();
+
+			Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0f);
+			Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+			Cell destinationCell = curFlowField.GetCellFromWorldPos(gameData.SpaceShips[0].Position);
+			curFlowField.CreateIntegrationField(destinationCell);
+
+			curFlowField.CreateFlowField();
+
+			gridDebug.DrawFlowField();
+		}
+	}
+}
