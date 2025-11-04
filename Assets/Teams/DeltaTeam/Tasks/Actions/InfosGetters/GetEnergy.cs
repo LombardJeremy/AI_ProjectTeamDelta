fbcb98f7b6d5e0e9b6a880d00ed7e7b8d3895f93
Volod_IA <volodiabussereau@gmail.com>
2025-11-04 15:24:57 +0100

    [ADD] Infos Getters Node

---
+++ Assets/Teams/DeltaTeam/Tasks/Actions/InfosGetters/GetEnergy.cs
@@ -0,0 +1,29 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+using UnityEngine;
+
+namespace DeltaTeam.Tasks.Actions
+{
+    [TaskCategory("DeltaTeam")]
+    public class GetEnergy : Action
+    {
+        public SharedDeltaController SharedController;
+        public SharedFloat EnergyValue;
+        public bool bUseOwnShip = false;
+
+        public override string OnDrawNodeText()
+        {
+            return  "Stores " + (bUseOwnShip ? "Own" : "Other") + " Energy in " + EnergyValue.Name;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            DeltaController controller = SharedController.Value;
+            
+            EnergyValue.Value = bUseOwnShip ? controller.OwnSpaceShip.Energy : controller.OtherSpaceShip.Energy;
+            
+            return TaskStatus.Success;
+        }
+    }
+}
---
+++ Assets/Teams/DeltaTeam/Tasks/Actions/InfosGetters/GetRemainingTime.cs
@@ -0,0 +1,28 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+using UnityEngine;
+
+namespace DeltaTeam.Tasks.Actions
+{
+    [TaskCategory("DeltaTeam")]
+    public class GetRemainingTime : Action
+    {
+        public SharedDeltaController SharedController;
+        public SharedFloat RemainingTime;
+
+        public override string OnDrawNodeText()
+        {
+            return  "Stores RemainingTime in " + RemainingTime.Name;
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            DeltaController controller = SharedController.Value;
+            
+            RemainingTime.Value = controller.GameData.timeLeft;
+            
+            return TaskStatus.Success;
+        }
+    }
+}
