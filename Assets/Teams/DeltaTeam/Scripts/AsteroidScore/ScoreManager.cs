6e4630207fb5743f4e54b626a2483d8c5103de18
jeremy-lombard <jeremylombard.mail@gmail.com>
2025-11-05 17:11:23 +0100

    Score Manager Done

--- Assets/Teams/DeltaTeam/Scripts/AsteroidScore/ScoreManager.cs
+++ Assets/Teams/DeltaTeam/Scripts/AsteroidScore/ScoreManager.cs
@@ -1,16 +1,54 @@
+using System;
+using System.Collections.Generic;
+using DoNotModify;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Dictionary<AsteroidView, float> asteroidScoreList;
+
+    private void Start()
     {
-        
+        foreach (var asteroid in GameManager.Instance.GetGameData().Asteroids)
+        {
+            asteroidScoreList[asteroid] = 0.0f;
+        }
+        CalculateAllWeightAtPlayerPos();
     }
 
-    // Update is called once per frame
-    void Update()
+    public Dictionary<AsteroidView, float> CalculateAllWeightAtPlayerPos()
     {
-        
+        foreach (var asteroid in GameManager.Instance.GetGameData().Asteroids)
+        {
+            CalculateAsteroidWeight(asteroid);
+        }
+        return asteroidScoreList;
+    }
+
+    public AsteroidView GetAsteroidWithLessWeight()
+    {
+        AsteroidView key = null;
+        float value = Int32.MaxValue;
+        foreach (var asteroidWithHisValue in asteroidScoreList)
+        {
+            if (asteroidWithHisValue.Value < value)
+            {
+                value = asteroidWithHisValue.Value;
+                key = asteroidWithHisValue.Key;
+            }
+        }
+        return key;
+    }
+
+    public void CalculateAsteroidWeight(AsteroidView asteroidToCalculate)
+    {
+        float maxWeight = 0.0f;
+        foreach (var asteroid in GameManager.Instance.GetGameData().Asteroids)
+        {
+            if(asteroid == asteroidToCalculate) continue;
+            maxWeight += Vector2.Distance(asteroid.Position, asteroidToCalculate.Position);
+        }
+        maxWeight += Vector2.Distance(asteroidToCalculate.Position, transform.position);
+        asteroidScoreList[asteroidToCalculate] = maxWeight;
     }
 }
