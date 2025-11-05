2a01b4dd0a1e535f5cd74a672b11f679da6bb385
Volod_IA <volodiabussereau@gmail.com>
2025-11-05 09:38:28 +0100

    [ADD] GetCurrentScoreAdvantage Node

---
+++ Assets/Teams/DeltaTeam/Tasks/Actions/InfosGetters/GetCurrentScoreAdvantage.cs
@@ -0,0 +1,33 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+using UnityEngine;
+
+namespace DeltaTeam.Tasks.Actions
+{
+    [TaskCategory("DeltaTeam")]
+    public class GetCurrentScoreAdvantage : Action
+    {
+        public SharedDeltaController SharedController;
+        public SharedFloat ScoreAdvantage;
+        public SharedFloat CombatScoreAdvantage;
+        public SharedFloat WaypointScoreAdvantage;
+
+        public override string OnDrawNodeText()
+        {
+            if (SharedController.Value == null) return "Missing Controller";
+            return base.OnDrawNodeText();
+        }
+
+        public override TaskStatus OnUpdate()
+        {
+            DeltaController controller = SharedController.Value;
+            
+            ScoreAdvantage.Value = controller.OwnSpaceShip.Score - controller.OtherSpaceShip.Score;
+            CombatScoreAdvantage.Value = controller.OwnSpaceShip.HitScore - controller.OtherSpaceShip.HitScore;
+            WaypointScoreAdvantage.Value = controller.OwnSpaceShip.WaypointScore - controller.OtherSpaceShip.WaypointScore;
+            
+            return TaskStatus.Success;
+        }
+    }
+}
