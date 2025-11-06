13fe233f31e074c9a5a83ce0f5222d7a9ec85c6e
jeremy-lombard <jeremylombard.mail@gmail.com>
2025-11-06 15:42:58 +0100

    Patch OPTIMISATION

--- Assets/Teams/DeltaTeam/Tasks/Actions/SetFlowFieldTarget.cs
+++ Assets/Teams/DeltaTeam/Tasks/Actions/SetFlowFieldTarget.cs
@@ -10,14 +10,21 @@ namespace DeltaTeam.Tasks.Actions
     {
         public SharedDeltaController SharedController;
         public SharedVector2 TargetPosition;
+        private Vector2 OldPos = new Vector2(int.MaxValue,int.MaxValue);
         
         public override TaskStatus OnUpdate()
         {
+            if (TargetPosition.Value == OldPos)
+            {
+                //Debug.Log("Same Pos For Flow Field");
+                return TaskStatus.Success;
+            }
             DeltaController controller = SharedController.Value;
             Vector2 playerPos = controller.OwnSpaceShip.Position;
             GridController curFlowField = controller.GetComponent<GridController>();
             if (curFlowField == null) return TaskStatus.Failure;
             curFlowField.CreateNewFlowField(TargetPosition.Value);
+            OldPos = TargetPosition.Value;
             return TaskStatus.Success;
         }
     }
