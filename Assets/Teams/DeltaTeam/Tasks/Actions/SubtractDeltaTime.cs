32b4415657a9ad20630df9da1368988a37397996
Volod_IA <volodiabussereau@gmail.com>
2025-11-07 08:58:28 +0100

    [ADD] SubtractDeltaTime Node

---
+++ Assets/Teams/DeltaTeam/Tasks/Actions/SubtractDeltaTime.cs
@@ -0,0 +1,19 @@
+﻿using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+using DoNotModify;
+using UnityEngine;
+
+namespace DeltaTeam.Tasks.Actions
+{
+    [TaskCategory("DeltaTeam")]
+    public class SubtractDeltaTime : Action
+    {
+        public SharedFloat variable;
+        
+        public override TaskStatus OnUpdate()
+        {
+            variable.Value -= Time.deltaTime;
+            return TaskStatus.Success;
+        }
+    }
+}
